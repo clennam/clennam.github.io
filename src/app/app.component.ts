8126b9dd34f3102525297ae3c75b338b2827dc36
clennam <kahandawalaaish@gmail.com>
2018-03-19 22:39:47 +0530

    Add some loading messages
    This commit adds some loading messages that will loop on the front page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Coming soon!';
+  index: number = 0;
+
+  statuses: Array<string> = [
+    'Coming soon!',
+    'Rolling for Initiative',
+    'Sipping tea',
+    'Driving at ninety down those country lanes',
+    'Attaining final form',
+    'Where we\'re going, we don\'t need roads!',
+    'I\'m Mister Meeseeks, look at me!'
+  ];
+
+  ngOnInit() {
+    this.statusLoop();
+  }
+
+  statusLoop() {
+    setTimeout(() => {
+      this.title = this.statuses[this.index];
+      this.index++;
+      console.log(this.index);
+      if (this.index > (this.statuses.length - 1)) {
+        this.index = 0;
+      }
+      this.statusLoop();
+    }, 3000);
+  }
+
 }
